9f1a85fac2f545bc593f6585a1e780193827bc86
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-02 01:11:36 +0200

    remove persistent menu

--- components/thread_settings.js
+++ components/thread_settings.js
@@ -7,23 +7,7 @@ module.exports = function(controller) {
     console.log('Configuring Facebook thread settings...');
     controller.api.messenger_profile.greeting("Siemandero!")
     controller.api.messenger_profile.get_started('sample_get_started_payload');
-    controller.api.messenger_profile.menu([
-        {
-            "type":"postback",
-            "title":"Hello",
-            "payload":"hello"
-        },
-        {
-            "type":"postback",
-            "title":"Help",
-            "payload":"help"
-        },
-        {
-          "type":"web_url",
-          "title":"Botkit Docs",
-          "url":"https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
-        },
-    ]);
+    
 
     console.log(controller.api.thread_settings.get_greeting());
 
