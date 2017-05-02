ef2873b2fee4e5a942671c428ca7d2d80877f389
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-02 23:12:27 +0200

    change payload

--- components/thread_settings.js
+++ components/thread_settings.js
@@ -5,5 +5,5 @@ var debug = require('debug')('botkit:thread_settings');
 module.exports = function(controller) {
 
     console.log('Configuring Facebook thread settings...');
-    controller.api.messenger_profile.get_started('get_started_payload');
+    controller.api.messenger_profile.get_started('GET_STARTED');
 }
