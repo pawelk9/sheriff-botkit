475d8053e01eac3e5d70ad0028f96c51888d5e01
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-03 00:24:40 +0200

    debug

---
+++ consts/payloads.js
@@ -0,0 +1,3 @@
+module.exports = {
+    GET_STARTED: "GET_STARTED"
+};
--- skills/greeting.js
+++ skills/greeting.js
@@ -1,6 +1,11 @@
 module.exports = function(controller) {
 
+    const payload = require('./../consts/payload');
+    
+
     controller.on('facebook_postback', function(bot, message) {
+        console.log(payload);
         bot.reply(message, 'Payload: ' + message.payload);
+
     });
 };
