6e3d0c9c441c877a0b4ac68c30833d023b038c11
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-03 22:06:54 +0200

    message driver

--- skills/onboarding.js
+++ skills/onboarding.js
@@ -7,22 +7,34 @@ module.exports = function (controller) {
     controller.on('facebook_postback', function (bot, message) {
         if (message.payload === payload.GET_STARTED) {
             bot.reply(message, 'Welcome message');
-            bot.reply(message, {attachment: receiveNotifications});
+            bot.reply(message, {
+                attachment: receiveNotifications
+            });
 
             controller.on('facebook_postback', function (bot, message) {
                 switch (message.payload) {
                     case payload.RECEIVE_NOTIFICATIONS_YES:
                         bot.reply(message, 'Store in DB');
                         bot.reply(message, "Yes message");
-                        bot.reply(message, {attachment: messageDriver});
+                        bot.reply(message, {
+                            attachment: messageDriver
+                        });
                         break;
 
                     case payload.RECEIVE_NOTIFICATIONS_NO:
                         bot.reply(message, 'No message');
-                        bot.reply(message, {attachment: messageDriver});
+                        bot.reply(message, {
+                            attachment: messageDriver
+                        });
                         break;
                 }
             });
         }
+
+        controller.on('facebook_postback', function (bot, message) {
+            if (message.payload === payload.MESSAGE_DRIVER_YES) {
+                bot.reply(message, "Sending message to driver");
+            }
+        });
     });
 };
