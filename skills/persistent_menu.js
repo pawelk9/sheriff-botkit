013d11ee2f04159147f191e08778044164a190af
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-03 10:34:53 +0200

    handle persitent menu postbacks

---
+++ skills/persistent_menu.js
@@ -0,0 +1,33 @@
+const payload = require('./../consts/payloads');
+
+module.exports = function (controller) {
+
+    controller.on('facebook_postback', function (bot, message) {
+        if (message.payload === payload.GET_STARTED) {
+            bot.reply(message, 'Welcome message');
+        }
+
+        switch (message.payload) {
+
+            case payload.SUBSCRIBE_TO_NOTIFICATIONS:
+                bot.reply(message, 'SUBSCRIBE_TO_NOTIFICATIONS');
+                break;
+
+            case payload.UNSUBSCRIBE_TO_NOTIFICATIONS:
+                bot.reply(message, 'UNSUBSCRIBE_TO_NOTIFICATIONS');
+                break;
+
+            case payload.BLOCK_DRIVER:
+                bot.reply(message, 'BLOCK_DRIVER');
+                break;
+
+            case payload.MESSAGE_DRIVER:
+                bot.reply(message, 'MESSAGE_DRIVER');
+                break;
+
+            case payload.GET_HELP:
+                bot.reply(message, 'GET_HELP');
+                break;
+        }
+    });
+};
