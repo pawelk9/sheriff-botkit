7a9d526e9ba014bcdfc1fbd7cbd052c3385c6afc
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-03 22:20:32 +0200

    refactor onboarding

--- skills/onboarding.js
+++ skills/onboarding.js
@@ -5,35 +5,31 @@ const messageDriver = require('./../messages/message_driver');
 module.exports = function (controller) {
 
     controller.on('facebook_postback', function (bot, message) {
+        
         if (message.payload === payload.GET_STARTED) {
             bot.reply(message, 'Welcome message');
             bot.reply(message, {
                 attachment: receiveNotifications
             });
+        }
 
-            controller.on('facebook_postback', function (bot, message) {
-                switch (message.payload) {
-                    case payload.RECEIVE_NOTIFICATIONS_YES:
-                        bot.reply(message, 'Store in DB');
-                        bot.reply(message, "Yes message");
-                        bot.reply(message, {
-                            attachment: messageDriver
-                        });
-                        break;
+        if (message.payload === payload.RECEIVE_NOTIFICATIONS_YES) {
+            bot.reply(message, 'Store in DB');
+            bot.reply(message, "Yes message");
+            bot.reply(message, {
+                attachment: messageDriver
+            });
+        }
 
-                    case payload.RECEIVE_NOTIFICATIONS_NO:
-                        bot.reply(message, 'No message');
-                        bot.reply(message, {
-                            attachment: messageDriver
-                        });
-                        break;
-                }
+        if (message.payload === payload.RECEIVE_NOTIFICATIONS_NO) {
+            bot.reply(message, 'No message');
+            bot.reply(message, {
+                attachment: messageDriver
             });
         }
 
         if (message.payload === payload.MESSAGE_DRIVER_YES) {
             bot.reply(message, "Sending message to driver");
         }
-        
     });
 };
