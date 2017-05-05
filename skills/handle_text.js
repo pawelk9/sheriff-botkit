1dd2ab4f624b5c58c674bd93874e9b67e5f51645
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-05 22:25:59 +0200

    log

--- skills/handle_text.js
+++ skills/handle_text.js
@@ -1,6 +1,7 @@
 module.exports = function (controller) {
 
     controller.on('message_received', function(bot, message){
+        console.log(message);
         bot.reply(message, 'Nice message');
     });
 
