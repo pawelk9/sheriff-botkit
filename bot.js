1cc6309bd227522295b6a6a44272589e9f0e2d94
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-08 23:46:04 +0200

    add log

--- bot.js
+++ bot.js
@@ -64,7 +64,9 @@ require(__dirname + '/components/onboarding.js')(controller);
 require(__dirname + '/components/plugin_dashbot.js')(controller);
 
 var normalizedPath = require("path").join(__dirname, "skills");
+console.log("#############" + normalizedPath);
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
+    console.log("./skills/" + file);
   require("./skills/" + file)(controller);
 });
 
