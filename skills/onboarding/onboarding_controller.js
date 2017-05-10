4b455ce8201bd9c8d4214b6ee3ece5a724d42fe1
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-10 22:01:29 +0200

    use switch statement

---
+++ skills/onboarding/onboarding_controller.js
@@ -0,0 +1,37 @@
+const payload = require('./../../consts/payloads');
+const receiveNotifications = require('./../../messages/receive_notifications');
+const messageDriver = require('./../../messages/message_driver');
+
+module.exports = function (controller) {
+
+    controller.on('facebook_postback', function (bot, message) {
+
+        switch (message.payload) {
+
+            case payload.GET_STARTED:
+                bot.reply(message, 'Welcome message');
+                bot.reply(message, {
+                    attachment: receiveNotifications
+                });
+
+            case payload.RECEIVE_NOTIFICATIONS_YES:
+                bot.reply(message, 'Store in DB');
+                bot.reply(message, "Yes message");
+                bot.reply(message, {
+                    attachment: messageDriver
+                });
+
+            case payload.RECEIVE_NOTIFICATIONS_NO:
+                bot.reply(message, 'No message');
+                bot.reply(message, {
+                    attachment: messageDriver
+                });
+
+            case payload.MESSAGE_DRIVER_YES:
+                bot.reply(message, "Alright, send me anything :)");
+
+            case payload.MESSAGE_DRIVER_NO:
+                bot.reply(message, "No? Here are main functionalities");
+        }
+    });
+};
