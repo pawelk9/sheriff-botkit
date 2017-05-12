a928f5d289b45bd278e1edd1651debd29c6572b8
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-12 21:06:50 +0200

    store yes no in one function

--- skills/shared/receive_notifications.js
+++ skills/shared/receive_notifications.js
@@ -1,15 +1,12 @@
 const payload = require('./../../consts/payloads');
 
-module.exports = {
-    method1: function (controller) {
-        controller.hears([payload.RECEIVE_NOTIFICATIONS_YES], 'facebook_postback', function (bot, message) {
-            bot.reply(message, 'Store in DB');
-            bot.reply(message, "Yes message");
-        });
-    },
-    method2: function (controller) {
-        controller.hears([payload.RECEIVE_NOTIFICATIONS_NO], 'facebook_postback', function (bot, message) {
-            bot.reply(message, 'No message');
-        });
-    }
+module.exports = function (controller) {
+    controller.hears([payload.RECEIVE_NOTIFICATIONS_YES], 'facebook_postback', function (bot, message) {
+        bot.reply(message, 'Store in DB');
+        bot.reply(message, "Yes message");
+    });
+
+    controller.hears([payload.RECEIVE_NOTIFICATIONS_NO], 'facebook_postback', function (bot, message) {
+        bot.reply(message, 'No message');
+    });
 }
