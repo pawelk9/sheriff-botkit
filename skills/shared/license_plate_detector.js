5606ad7d739dbc6d4ab2613b057d77f69c967b08
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-13 00:15:59 +0200

    fix whitespace

--- skills/shared/license_plate_detector.js
+++ skills/shared/license_plate_detector.js
@@ -1,5 +1,5 @@
 module.exports = (controller) => {
-    controller.hears(['^[A-Z0-9]{2,3}\s?[A-Z0-9]{3,5}$'], 'text_received', function (bot, message) {
+    controller.hears(['^[A-Z0-9]{2,3}\\s?[A-Z0-9]{3,5}$'], 'text_received', function (bot, message) {
         bot.reply(message, "O, tablica!");
     });
 }
