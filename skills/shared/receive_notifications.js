70f927e8e5dc0a1bdd89198a89e4a64083da1e20
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-12 21:00:24 +0200

    change export

--- skills/shared/receive_notifications.js
+++ skills/shared/receive_notifications.js
@@ -1,14 +1,15 @@
 const payload = require('./../../consts/payloads');
 
-module.exports = function (controller) {
-    controller.hears([payload.RECEIVE_NOTIFICATIONS_YES], 'facebook_postback', function (bot, message) {
-        bot.reply(message, 'Store in DB');
-        bot.reply(message, "Yes message");
-    });
-}
-
-module.exports = function (controller) {
-    controller.hears([payload.RECEIVE_NOTIFICATIONS_NO], 'facebook_postback', function (bot, message) {
-        bot.reply(message, 'No message');
-    });
+module.exports = {
+    function (controller) {
+        controller.hears([payload.RECEIVE_NOTIFICATIONS_YES], 'facebook_postback', function (bot, message) {
+            bot.reply(message, 'Store in DB');
+            bot.reply(message, "Yes message");
+        });
+    },
+    function (controller) {
+        controller.hears([payload.RECEIVE_NOTIFICATIONS_NO], 'facebook_postback', function (bot, message) {
+            bot.reply(message, 'No message');
+        });
+    }
 }
