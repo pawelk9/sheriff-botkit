1377ad84b72237568a52704297a7804c9a3543e5
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-13 21:26:39 +0200

    test add question

--- skills/shared/type_license_plate.js
+++ skills/shared/type_license_plate.js
@@ -9,7 +9,7 @@ module.exports = (controller) => {
             convo.addMessage({
                 text: "Nie mogę rozponać tablicy. Wpisz w formacie XX XXXXX lub XXX XXXX.",
                 action: "default",
-            },"bad_response");
+            }, "bad_response");
 
             convo.ask("Super! Podaj swoją tablicę rejestracyjną", [{
                     pattern: bot.utterances.yes,
@@ -26,6 +26,40 @@ module.exports = (controller) => {
                 }
             ]);
             convo.activate();
+
+            convo.addQuestion('Shall we proceed Say YES, NO or DONE to quit.', [{
+                    pattern: 'done',
+                    callback: function (response, convo) {
+                        convo.say('OK you are done!');
+                        convo.next();
+                    }
+                },
+                {
+                    pattern: bot.utterances.yes,
+                    callback: function (response, convo) {
+                        convo.say('Great! I will continue...');
+                        // do something else...
+                        convo.next();
+
+                    }
+                },
+                {
+                    pattern: bot.utterances.no,
+                    callback: function (response, convo) {
+                        convo.say('Perhaps later.');
+                        // do something else...
+                        convo.next();
+                    }
+                },
+                {
+                    default: true,
+                    callback: function (response, convo) {
+                        // just repeat the question
+                        convo.repeat();
+                        convo.next();
+                    }
+                }
+            ], {}, 'repeat');
         });
     });
 };
