13de4c9094a5b017a8d4358ea98920c2796fbcb4
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-13 10:27:14 +0200

    use conversation

--- skills/onboarding/get_started.js
+++ skills/onboarding/get_started.js
@@ -3,9 +3,9 @@ const receiveNotifications = require('./../../messages/receive_notifications');
 
 module.exports = (controller) => {
     controller.hears([payload.GET_STARTED], 'facebook_postback', function (bot, message) {
-        bot.reply(message, 'Wilkommen!');
-        bot.reply(message, {
-            attachment: receiveNotifications
+        bot.startConversation(message, function (err, convo) {
+            convo.say("Cześć! Jestem Szeryf!");
+            convo.say('Have a nice day!');
         });
     });
 }
