fcab9366f11e25ef4669738c9f791b1a42d1f989
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-13 11:37:09 +0200

    add subsciption to onboarding

--- skills/onboarding/get_started.js
+++ skills/onboarding/get_started.js
@@ -1,11 +1,10 @@
 const payload = require('./../../consts/payloads');
-const receiveNotifications = require('./../../messages/receive_notifications');
 
 module.exports = (controller) => {
     controller.hears([payload.GET_STARTED], 'facebook_postback', function (bot, message) {
         bot.startConversation(message, function (err, convo) {
             convo.say("Cześć! Jestem Szeryf!");
-            convo.say('Have a nice day!');
+            controller.trigger('subscribe_to_notifications', [bot, message]);
         });
     });
 }
