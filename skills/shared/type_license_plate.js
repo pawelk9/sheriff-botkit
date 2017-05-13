c7a56ba7de00df5d46689e04bf7a1a7061361887
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-13 20:51:01 +0200

    test

--- skills/shared/type_license_plate.js
+++ skills/shared/type_license_plate.js
@@ -4,6 +4,31 @@ module.exports = (controller) => {
 
         bot.createConversation(message, function (err, convo) {
 
+            convo.addMessage({
+                text: "How wonderful.",
+            }, "valid_license_plate");
+
+            convo.ask("Super! Podaj swoją tablicę rejestracyjną", [{
+                    pattern: bot.utterances.yes,
+                    callback: function (response, convo) {
+                        convo.gotoThread("valid_license_plate");
+                    },
+                },
+                {
+                    pattern: bot.utterances.no,
+                    callback: function (response, convo) {
+                        convo.gotoThread('no_thread');
+                    },
+                },
+                {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.gotoThread('bad_response');
+                    },
+                }
+            ]);
+
+            convo.activate();
 
             convo.say('Hello!');
             convo.say('Have a nice day!');
