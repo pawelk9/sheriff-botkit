257b6480fc0f63fa66bf0fcdc5d6e794d1f5769a
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-14 21:31:14 +0200

    refactor

--- api/user_profile_api.js
+++ api/user_profile_api.js
@@ -1,14 +1,10 @@
-const rp = require('request-promise');
-
-var options = {
-
-        uri: "https://graph.facebook.com/v2.6/" + '1465070823516350',
+module.exports = (messangerId) => {
+    return {
+        uri: "https://graph.facebook.com/v2.6/" + messangerId,
         qs: {
             fields: "first_name,last_name,profile_pic,locale,timezone,gender",
             access_token: process.env.page_token
         },
         json: true
-    
+    }
 };
-
-module.exports = rp(options);
--- skills/onboarding/get_started.js
+++ skills/onboarding/get_started.js
@@ -1,12 +1,13 @@
 const payload = require('./../../consts/payloads');
 const User = require('./../../persistence/models/user');
 const getUserProfile = require('./../../api/user_profile_api');
+const rp = require('request-promise');
 
 module.exports = (controller) => {
     controller.hears([payload.GET_STARTED], 'facebook_postback', function (bot, message) {
         bot.startConversation(message, function (err, convo) {
 
-            getUserProfile.then(function (body) {
+            rp(getUserProfile(message.user)).then(function (body) {
                     console.log('###', body);
                 })
                 .catch(function (err) {
