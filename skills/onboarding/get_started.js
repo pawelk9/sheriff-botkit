2fe1850108f9c24a667c62c100d4e3cfbc8d0f6d
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-18 20:36:59 +0200

    save user details

--- skills/onboarding/get_started.js
+++ skills/onboarding/get_started.js
@@ -9,58 +9,32 @@ module.exports = (controller) => {
 
             User.alreadyRegistered(message.user)
                 .then(obj => {
-                    console.log('alreadyRegistered', obj);
                     convo.say("Witaj ponownie!");
                 })
                 .catch(obj => {
-                    console.log('catch', obj);
+                    rp(getUserProfile(message.user))
+                        .then((body) => {
+                            let user = new User({
+                                messangerId: message.user,
+                                first_name: body.first_name,
+                                last_name: body.last_name,
+                                profile_pic: body.profile_pic,
+                                locale: body.locale,
+                                timezone: body.timezone,
+                                gender: body.gender
+                            });
+                            user.save().then(() => {}).catch(() => {});
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            let user = new User({
+                                messangerId: message.user
+                            });
+                            user.save().then(() => {}).catch(() => {});
+                        });
                     convo.say("Cześć! Jestem Szeryf!");
                     controller.trigger('subscribe_to_notifications', [bot, message]);
                 });
-
-            // var promise = User.count({
-            //     messangerId: message.user
-            // }).exec();
-
-            // promise.then(count => {
-            //         if (count > 0) {
-            //             convo.say("Witaj ponownie!");
-            //         } else {
-            //             rp(getUserProfile(message.user)).then((body) => {
-            //                     let user = new User({
-            //                         messangerId: message.user,
-            //                         first_name: body.first_name,
-            //                         last_name: body.last_name,
-            //                         profile_pic: body.profile_pic,
-            //                         locale: body.locale,
-            //                         timezone: body.timezone,
-            //                         gender: body.gender
-            //                     });
-
-            //                     user.save((err) => {
-            //                         if (err) {
-            //                             console.log(err);
-            //                             return;
-            //                         };
-            //                     });
-            //                 })
-            //                 .catch((err) => {
-            //                     console.log(err);
-            //                     let user = new User({
-            //                         messangerId: message.user
-            //                     });
-            //                     user.save((err) => {
-            //                         if (err) {
-            //                             console.log(err);
-            //                             return;
-            //                         };
-            //                     });
-            //                 });
-            //             convo.say("Cześć! Jestem Szeryf!");
-            //             controller.trigger('subscribe_to_notifications', [bot, message]);
-            //         }
-            //     })
-            //     .catch();
         });
     });
 }
