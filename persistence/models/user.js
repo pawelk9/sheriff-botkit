bcfb6f557ee98dc413f1b12302c775beaf8e4e5a
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-18 21:09:19 +0200

    use pre hook for user details

--- persistence/models/user.js
+++ persistence/models/user.js
@@ -21,9 +21,20 @@ const userSchema = new Schema({
   }
 });
 
-userSchema.pre('save', next => {
-  console.log(this);
+userSchema.pre('save', function (next) {
 
+  rp(getUserProfile(this.messangerId))
+    .then(body => {
+      this.first_name = body.first_name;
+      this.last_name = body.last_name;
+      this.profile_pic = body.profile_pic;
+      this.locale = body.locale;
+      this.timezone = body.timezone;
+      this.gender = body.gender;
+    })
+    .catch(err => {
+      console.log(err);
+    });
   next();
 });
 
