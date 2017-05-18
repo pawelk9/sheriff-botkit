1371a5d503fe9378292f9dc5bfc7a6211fdba849
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-18 23:04:35 +0200

    use es6 functions

--- skills/shared/subscribe_to_notifications.js
+++ skills/shared/subscribe_to_notifications.js
@@ -2,9 +2,9 @@ const receiveNotifications = require('./../../messages/receive_notifications');
 const payload = require('./../../consts/payloads');
 
 module.exports = (controller) => {
-    controller.on('subscribe_to_notifications', function (bot, message) {
+    controller.on('subscribe_to_notifications', (bot, message) => {
 
-        bot.createConversation(message, function (err, convo) {
+        bot.createConversation(message, (err, convo) => {
             convo.addMessage({
                 text: 'Spoko. Możesz zapisać się w każdej chwili w menu.',
             }, 'no_thread');
@@ -15,23 +15,23 @@ module.exports = (controller) => {
             }, 'bad_response');
 
             convo.addQuestion({
-                attachment: receiveNotifications("Chcesz otrzymywać powiadomienia?", "Tak", "Nie")
+                attachment: receiveNotifications('Chcesz otrzymywać powiadomienia?', 'Tak', 'Nie')
             }, [{
                     pattern: payload.RECEIVE_NOTIFICATIONS_YES,
-                    callback: function (response, convo) {
+                    callback: (response, convo) => {
                         convo.stop();
                         controller.trigger('type_license_plate', [bot, message]);
                     },
                 },
                 {
                     pattern: payload.RECEIVE_NOTIFICATIONS_NO,
-                    callback: function (response, convo) {
+                    callback: (response, convo) => {
                         convo.gotoThread('no_thread');
                     },
                 },
                 {
                     default: true,
-                    callback: function (response, convo) {
+                    callback: (response, convo) => {
                         convo.gotoThread('bad_response');
                     },
                 }
