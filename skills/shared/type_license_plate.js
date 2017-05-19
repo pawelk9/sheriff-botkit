e665930b7493d0efe5ca0d5396e7d8ea2a38ecbc
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-19 23:42:24 +0200

    reformat document

--- skills/shared/type_license_plate.js
+++ skills/shared/type_license_plate.js
@@ -1,37 +1,37 @@
 module.exports = (controller) => {
-    controller.on('type_license_plate', function (bot, message) {
+  controller.on('type_license_plate', function (bot, message) {
 
-        bot.createConversation(message, function (err, convo) {
+    bot.createConversation(message, function (err, convo) {
 
-            convo.addQuestion('Podaj swoją tablicę rejestracyjną', [{
-                    pattern: bot.utterances.yes,
-                    callback: function (response, convo) {
-                        convo.say('Super. Już zapisuję!');
-                        bot.replyWithTyping(message, 'Zapisane! Wyślę do Ciebie wiadomość jak tylko coś dostanę :)');
-                        convo.next();
-                    },
-                },
-                {
-                    default: true,
-                    callback: function (response, convo) {
-                        convo.say('Nie mogę rozponać tablicy. Wpisz w formacie XX XXXXX lub XXX XXXX.');
-                        convo.repeat();
-                        convo.next();
-                    },
-                }
-            ], {}, 'default');
+      convo.addQuestion('Podaj swoją tablicę rejestracyjną', [{
+          pattern: bot.utterances.yes,
+          callback: function (response, convo) {
+            convo.say('Super. Już zapisuję!');
+            bot.replyWithTyping(message, 'Zapisane! Wyślę do Ciebie wiadomość jak tylko coś dostanę :)');
+            convo.next();
+          },
+        },
+        {
+          default: true,
+          callback: function (response, convo) {
+            convo.say('Nie mogę rozponać tablicy. Wpisz w formacie XX XXXXX lub XXX XXXX.');
+            convo.repeat();
+            convo.next();
+          },
+        }
+      ], {}, 'default');
 
-            convo.activate();
+      convo.activate();
 
-            convo.on('end', function (convo) {
-                if (convo.status === 'completed') {
-                    var res = convo.extractResponses();
-                    console.log(res);
+      convo.on('end', function (convo) {
+        if (convo.status === 'completed') {
+          var res = convo.extractResponses();
+          console.log(res);
 
-                } else {
-                    console.log('Something failed', convo.status);
-                }
-            });
-        });
+        } else {
+          console.log('Something failed', convo.status);
+        }
+      });
     });
+  });
 };
