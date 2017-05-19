32a786b8f1194cab23026546d540195bb41fdc2c
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-19 23:48:05 +0200

    format document

--- core/validators/license_plate_validator.js
+++ core/validators/license_plate_validator.js
@@ -1,18 +1,17 @@
 const LICENSE_PLATE_PATTERNS = {
-    PL: /[A-Z0-9]{2,3}\s?[A-Z0-9]{3,5}/
+  PL: /[A-Z0-9]{2,3}\s?[A-Z0-9]{3,5}/
 };
 
 module.exports = (input) => {
 
-    input = input.trim().toUpperCase();
-	input = input.replace(" ","");
-	input = input.replace("\r","");
-	input = input.replace("\n","")
-    input = input.replace(/\W/g, '');
+  input = input.trim().toUpperCase();
+  input = input.replace(' ', '');
+  input = input.replace('\r', '');
+  input = input.replace('\n', '');
+  input = input.replace(/\W/g, '');
 
-    if (LICENSE_PLATE_PATTERNS.PL.test(input)) {
-        return true;
-    }
-    return false;
+  if (LICENSE_PLATE_PATTERNS.PL.test(input)) {
+    return true;
+  }
+  return false;
 };
-
