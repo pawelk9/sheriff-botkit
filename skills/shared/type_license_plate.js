478043c3034be09afe89922ba003344ee476dc78
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-20 00:08:49 +0200

    use es6, add logger

--- skills/shared/type_license_plate.js
+++ skills/shared/type_license_plate.js
@@ -1,21 +1,20 @@
 const platePattern = require('./../../core/validators/license_plate_patterns');
+const logger = require('../../utils/logger');
 
 module.exports = (controller) => {
-  controller.on('type_license_plate', function (bot, message) {
-
-    bot.createConversation(message, function (err, convo) {
+  controller.on('type_license_plate', (bot, message) => {
+    bot.createConversation(message, (err, convo) => {
 
       convo.addQuestion('Podaj swoją tablicę rejestracyjną', [{
           pattern: platePattern.PL,
-          callback: function (response, convo) {
-            convo.say('Super. Już zapisuję!');
+          callback: (response, convo) => {
             bot.replyWithTyping(message, 'Zapisane! Wyślę do Ciebie wiadomość jak tylko coś dostanę :)');
             convo.next();
           },
         },
         {
           default: true,
-          callback: function (response, convo) {
+          callback: (response, convo) => {
             convo.say('Nie mogę rozponać tablicy. Wpisz w formacie XX XXXXX lub XXX XXXX.');
             convo.repeat();
             convo.next();
@@ -25,13 +24,12 @@ module.exports = (controller) => {
 
       convo.activate();
 
-      convo.on('end', function (convo) {
+      convo.on('end', convo => {
         if (convo.status === 'completed') {
           var res = convo.extractResponses();
-          console.log(res);
-
+          logger.info(res);
         } else {
-          console.log('Something failed', convo.status);
+          logger.error(`License plate conversation failed. ${convo.status}`);
         }
       });
     });
