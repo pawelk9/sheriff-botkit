d9e57d6f9abd60c2b580d17903af4840c2534568
Paweł Kozłowski <pawelk9@gmail.com>
2017-05-23 21:22:14 +0200

    test ngrok

--- skills/messages/text_received.js
+++ skills/messages/text_received.js
@@ -2,7 +2,7 @@ const messageDriver = require('./../../messages/message_driver');
 module.exports = (controller) => {
 
     controller.on('text_received', function (bot, message) {
-        bot.reply(message, 'Nice text dude!');
+        bot.reply(message, 'Nice text ziomeczku eloszka!');
         // bot.reply(message, {
             // attachment: messageDriver
         // });
